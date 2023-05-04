7a3e6868fe76ad2dce817e7892554dc0204d8df5
Orranj <126442780+Orranj@users.noreply.github.com>
2023-05-04 21:51:49 +0800

    Add files via upload

---
+++ Calcium-1/script.js
@@ -0,0 +1,29 @@
+var cursor = document.querySelector('.cursor');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+  cursor2.style.left = e.clientX + 'px';
+  cursor2.style.top = e.clientY + 'px';
+});
+
+var cntnr = document.getElementById('Tcontainer');
+var title = document.getElementById('title');
+var lngth = title.textContent.length;
+var fntsz = 335 / lngth;
+title.style.fontSize = fntsz + 'vw';
+
+var groupwun = document.getElementById('groupwun');
+var lngth2 = groupwun.textContent.length;
+var fntsiz = 90 / lngth;
+groupwun.style.fontSize = fntsiz + 'vw';
+
+const mySlider = new DraggableSlider('example');
+
+$(document).ready(function() {
+  $('#biotext').click(function() {
+    $('#biomore').toggle('default', function() {
+      // Animation complete.
+    });
+  });
+});
