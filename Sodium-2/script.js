9726ad51a0d7f44a0ae84cc7a2aba1391f77d962
Zildjian Laid <126549677+Zild-Kyusi@users.noreply.github.com>
2023-05-10 19:54:27 +0800

    Add files via upload

---
+++ Sodium-2/script.js
@@ -0,0 +1,15 @@
+function Morb(){
+    document.getElementById("Egg").style.display = "inline-block";
+    alert("You have found the easter egg!")
+}
+
+function reveal(){
+    document.getElementById("Fl").style.display = "inline-block";
+    document.getElementById("Ev").style.display = "inline-block";
+    document.getElementById("Zi").style.display = "inline-block";
+    document.getElementById("Ba").style.display = "inline-block";
+    document.getElementById("Sa").style.display = "inline-block";
+    document.getElementById("Ma").style.display = "inline-block";
+    document.getElementById("Cac").style.top = "-60%";
+    document.getElementById("Dirt").style.top = "-65%";
+}
