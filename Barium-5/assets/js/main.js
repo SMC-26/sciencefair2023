e4a428ed2b53bae7d4e25889045343879a5209c3
revgit <87372988+atoregit@users.noreply.github.com>
2023-05-11 19:20:28 +0800

    hope i didnt break anything :skull:

--- Barium-5/assets/js/main.js
+++ Barium-5/assets/js/main.js
@@ -19,28 +19,6 @@
       return document.querySelector(el)
     }
   }
-  
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
 
   /**
    * Hero type effect
